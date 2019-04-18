c97e76720da0fb52a163e6d5c111e73d6cbf8fab
WittBulter <nanazuimeng123@gmail.com>
2019-04-18 15:55:34 +0800

    feat: add script

---
+++ install_want/index.sh
@@ -0,0 +1,37 @@
+BIN='want () { curl -sd "$*" https://want.now.sh | bash -s --; }'
+CYAN="$(tput setaf 6 2>/dev/null || echo '')"
+NO="$(tput sgr0 2>/dev/null || echo '')"
+LINT="$(tput smul 2>/dev/null || echo '')"
+BOLD="$(tput bold 2>/dev/null || echo '')"
+
+install_to_zsh() {
+  if ! grep -q "${BIN}" ~/.zshrc; then
+    echo "${BIN}" >> ~/.zshrc
+  fi
+}
+
+install_to_bash() {
+  if ! grep -q "${BIN}" ~/.bashrc; then
+    echo "${BIN}" >> ~/.bashrc
+  fi
+}
+
+success() {
+  echo "Everything is ready, run <${CYAN}${BOLD}want${NO}> to start."
+  echo ""
+}
+
+if test -s ~/.zshrc; then
+  install_to_zsh
+  success
+  exit
+fi
+
+if test -s ~/.bashrc; then
+  install_to_bash
+  success
+  exit
+fi
+
+echo "Unsupported systems."
+echo ""
